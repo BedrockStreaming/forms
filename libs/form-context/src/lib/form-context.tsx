f7255e8e815c726e5f3df72f881ecf84009b3040
hpierre74 <pierrehuyghe.pro@gmail.com>
2022-01-23 15:52:54 +0100

    feat(libs/form-context): create context api wrapper

---
+++ libs/form-context/src/lib/form-context.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface FormProviderProps {}
+
+export function FormProvider(props: FormProviderProps) {
+  return (
+    <div>
+      <h1>Welcome to FormProvider!</h1>
+    </div>
+  );
+}
+
+export default FormProvider;
