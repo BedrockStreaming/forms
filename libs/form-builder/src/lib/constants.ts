88c5b56e18a928d3c649c2e2a3d7cb4e774905aa
hpierre74 <pierrehuyghe.pro@gmail.com>
2022-03-14 00:39:13 +0100

    ci(pre-release): fix pipeline

--- libs/form-builder/src/lib/constants.ts
+++ libs/form-builder/src/lib/constants.ts
@@ -4,7 +4,6 @@ interface DeFaultRulesNames {
   [key: string]: string;
 }
 
-// Todo only restrict the react-hook-form rules when those are used in a validation rule list
 export const DEFAULT_RULES_NAMES: DeFaultRulesNames = {
   min: 'min',
   max: 'max',
