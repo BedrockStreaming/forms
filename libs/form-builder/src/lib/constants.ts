453f65e0f165df30135d88a70219c0272a80de09
hpierre74 <pierrehuyghe.pro@gmail.com>
2022-03-14 00:19:50 +0100

    fix(libs/form-builder): add information about rule restrictions

--- libs/form-builder/src/lib/constants.ts
+++ libs/form-builder/src/lib/constants.ts
@@ -4,6 +4,7 @@ interface DeFaultRulesNames {
   [key: string]: string;
 }
 
+// Todo only restrict the react-hook-form rules when those are used in a validation rule list
 export const DEFAULT_RULES_NAMES: DeFaultRulesNames = {
   min: 'min',
   max: 'max',
