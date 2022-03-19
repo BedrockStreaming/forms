115e63a35b596e34a865229dc944712b1b9fc481
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2022-03-19 11:10:52 +0100

    ci(pre-release): pre-release ci (#73)

--- libs/form-builder/src/lib/constants.ts
+++ libs/form-builder/src/lib/constants.ts
@@ -4,6 +4,7 @@ interface DeFaultRulesNames {
   [key: string]: string;
 }
 
+// TODO: only restrict rules used with form-validation-rule-list
 export const DEFAULT_RULES_NAMES: DeFaultRulesNames = {
   min: 'min',
   max: 'max',
